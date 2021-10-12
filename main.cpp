e58cc37a75d7ad49dcf96443f5a03cc2bccad8ba
SeresHotes <sereshotes@gmail.com>
2021-10-12 23:29:26 +0300

    Maybe working project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#ifndef VERSION
+#define VERSION 1
+#endif
+
+using namespace std;
+
+
+int main() {
+    cout << "Version: " << VERSION << endl;
+    cout << "Hello, World!" << endl;    
+    return 0;
+}
